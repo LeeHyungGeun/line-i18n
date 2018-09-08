d32249390ff6f74976c827a10236c6f052fc4d70
Ken Lee <ken.lee@fairfaxmedia.com.au>
2018-09-08 17:46:03 +1000

    add messages

---
+++ src/i18n/messages/index.js
@@ -0,0 +1,31 @@
+import ja_JP from './ja_JP';
+import en_US from './en_US';
+import ko_KR from './ko_KR';
+import zh_CN from './zh_CN';
+import zh_TW from './zh_TW';
+import fr_FR from './fr_FR';
+export default {
+    // Japanese
+    ja_JP,
+    'ja-JP': ja_JP,
+    ja: ja_JP,
+    // English
+    en_US,
+    'en-US': en_US,
+    en: en_US,
+    // Korean
+    ko_KR: ko_KR,
+    'ko-KR': ko_KR,
+    ko: ko_KR,
+    // Chinese Simplified
+    zh_CN,
+    'zh-CN': zh_CN,
+    // Chinese Traditional
+    zh_TW,
+    'zh-TW': zh_TW,
+    zh: zh_TW,
+    // French
+    fr_FR,
+    'fr-FR': fr_FR,
+    fr: fr_FR
+};
---
+++ src/i18n/messages/ja_JP.js
@@ -0,0 +1,5 @@
+export default {
+    name: 'LeeHyungGeun',
+    occupation: 'Front End Developer',
+    language: 'Japanese',
+};
