18fa4c7dd6e46538dddde1c85e75fbfc0de4f991
Ken Lee <ken.lee@fairfaxmedia.com.au>
2018-09-08 17:48:47 +1000

    clean code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import i18n from './i18n';
 
+@i18n
 class App extends Component {
   render() {
+    const { M } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {M.name}
       </div>
     );
   }
